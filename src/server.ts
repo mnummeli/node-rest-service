c0c89916a8245e0868aaffc74533cc80101d4d50
Mikko Nummelin <mnummeli@gmail.com>
2025-09-26 13:25:18 +0300

    JS->TS

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,17 @@
 
 import os from 'node:os';
 import express from 'express';
+import type { Application, Request, Response } from 'express';
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+const PORT: number = Number(process.env.PORT) || 3000;
+const app: Application = express();
 
-let id = 0;
+let id: number = 0;
 
-app.get('/greeting', (req, res) => {
-	const parsedUrl = new URL(`http://${os.hostname()}${req.url}`);
-	const searchParams = parsedUrl.searchParams;
-	const name = searchParams.get('name') || 'World';
+app.get('/greeting', (req: Request, res: Response) => {
+	const parsedUrl: URL = new URL(`http://${os.hostname()}${req.url}`);
+	const searchParams: URLSearchParams = parsedUrl.searchParams;
+	const name: string = searchParams.get('name') || 'World';
 	res.setHeader('Content-Type', 'application/json');
 	res.json({ id: ++id, content: `Hello ${name}!` });
 });
